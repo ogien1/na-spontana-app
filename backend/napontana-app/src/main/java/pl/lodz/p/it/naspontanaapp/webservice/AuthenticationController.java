60bd4ff86885e42921f513ee1214a251ad11dfb3
Marcin Ostrowski <marcinostry@gmail.com>
2016-11-30 10:27:44 +0100

    modyfikacja kontrolerow, testy

--- backend/napontana-app/src/main/java/pl/lodz/p/it/naspontanaapp/webservice/AuthenticationController.java
+++ backend/napontana-app/src/main/java/pl/lodz/p/it/naspontanaapp/webservice/AuthenticationController.java
@@ -1,10 +1,36 @@
 package pl.lodz.p.it.naspontanaapp.webservice;
 
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import pl.lodz.p.it.naspontanaapp.service.ActivityCreationManager;
 
 /**
  * Created by 'Jakub Dziworski' on 30.11.16
  */
+@Transactional(value = TxType.REQUIRES_NEW)
 @RestController
+@RequestMapping("/auth")
 public class AuthenticationController {
+	
+	@Autowired
+	ActivityCreationManager activityCreationManager;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test(){
+		LOGGER.info("test|START - wywolano z parametrami: " );
+//		activityCreationManager.doSth();
+		LOGGER.info("test|STOP - zapisano: ");
+		return "auth test ok";
+	}
+	
 }
